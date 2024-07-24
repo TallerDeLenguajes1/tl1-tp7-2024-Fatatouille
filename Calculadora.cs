0e7c2ca511fb971fd3ddbff4c66f292895813884
Gimenez Francisco Miguel <franciscomguelgimenez708@gmail.com>
2024-07-24 15:57:53 -0300

    Interfaz hecha

--- Calculadora.cs
+++ Calculadora.cs
@@ -1,16 +1,18 @@
 namespace EspacioCalculadora{
+
     public class Calculadora{
+
         private double dato = 0;
         public double resultado { get => dato;}
 
-        void Sumar(double termino){ dato+=termino; }
-        void Restar(double termino){ dato-=termino; }
-        void Multiplicar(double termino){ dato*=termino; }
-        void Dividir(double termino){
+        public void Sumar(double termino){ dato+=termino; }
+        public void Restar(double termino){ dato-=termino; }
+        public void Multiplicar(double termino){ dato*=termino; }
+        public void Dividir(double termino){
             if(termino!=0){
                 dato/=termino;
             }
         }
-        void Limpiar(double termino){ dato=0; }
+        public void Limpiar(){ dato=0; }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,55 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using EspacioCalculadora;
 
+Calculadora calculadora = new Calculadora();
+int opcion;
+Console.WriteLine("Ingrese un numero: ");
+double valor = Convert.ToDouble(Console.ReadLine());
+calculadora.Sumar(valor);
+bool opera=true;
+
+while(opera){
+    if(calculadora.resultado == 0){
+        Console.WriteLine("Ingrese un numero: ");
+        valor = Convert.ToDouble(Console.ReadLine());
+    }else{
+        Console.WriteLine("Que operacion quieres hacer?");
+        Console.WriteLine("1: Sumar");
+        Console.WriteLine("2: Restar");
+        Console.WriteLine("3: Multiplicar");
+        Console.WriteLine("4: Dividir");
+        Console.WriteLine("5: Limpiar");
+        Console.WriteLine("6: Salir");
+
+        opcion= Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Ingrese un numero: ");
+        valor = Convert.ToDouble(Console.ReadLine());
+        
+        switch (opcion)
+        {
+            case 1:
+                calculadora.Sumar(valor);
+                break;
+            case 2:
+                calculadora.Restar(valor);
+                break;
+            case 3:
+                calculadora.Multiplicar(valor);
+                break;
+            case 4:
+                calculadora.Dividir(valor);
+                break;
+            case 5:
+                calculadora.Limpiar();
+                break;
+            case 6:
+                opera=false;
+                break;
+            default:
+                Console.WriteLine("Ingrese una opcion.");
+                break;
+        }
+        Console.WriteLine($"El resultado es {calculadora.resultado}");
+    }
+}    
