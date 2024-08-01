3f4ecdf7747d3d8b7255e96c8d2f5f098506cf11
Gimenez Francisco Miguel <franciscomguelgimenez708@gmail.com>
2024-08-01 20:58:22 -0300

    Enum bien hecho

--- Empleados.cs
+++ Empleados.cs
@@ -6,7 +6,7 @@ namespace Empleados{
         private char EC; //C: Casado, S: Soltero
         private DateTime ingreso;
         private double sueldo;
-        private enum Cargo
+        public enum cargo
         {
             Auxiliar,
             Administrativo,
@@ -14,6 +14,15 @@ namespace Empleados{
             Especialista,
             Investigador
         };
+        private cargo cargoE;
+
+        public string Nombre { get => nombre; set => nombre=value};
+        public string Apellido { get => apellido; set => apellido=value};
+        public DateTime Nacimiento { get => nombre; set => nombre=value};
+        public char EstadoC { get => EC; set => EC=value}; //C: Casado, S: Soltero
+        public DateTime Ingreso { get => ingreso; set => ingreso=value};
+        public double Sueldo { get => sueldo; set => sueldo=value};
+        public cargo CargoE { get => cargoE; set => cargoE=value};
 
         private int Antiguedad(){
             int antiguedad =  DateTime.Today.Year - ingreso.Year;
